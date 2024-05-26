3bd0fc2ac82183b3df9e5c0221d21fc1651ecf23
Monki12 <ankitabagal70@gmail.com>
2024-05-26 12:14:19 +0530

    landing page base

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-document.querySelector(".test").innerHTML= "yes";
+// Dropdown list
+let dropdownbtn = document.querySelector("#droptext");
+let list = document.querySelector("#list");
+let dropicon = document.querySelector("#dropicon");
+
+
+dropdownbtn.onclick=() =>{
+    list.classList.toggle("show");
+    dropicon.classList.toggle("show");
+};
+
+//voice search and icon animation
+
+let voicebtn= document.querySelector("#micbtn");
+let voiceicon= document.querySelector("#mic");
+voicebtn.onclick= () => {
+    voiceicon.classList.toggle("recording");
+};
